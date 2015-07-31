522cbdc3aa34969714dc28500e77347fb72ef690
eandino <eandino@PC-04.hdm.edu>
2015-07-31 11:01:26 -0600

    modelo asignatura

---
+++ gestionEscolar/src/modelo/MtAsignatura.java
@@ -0,0 +1,40 @@
+package modelo;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+
+public class MtAsignatura extends AbstractTableModel {
+	
+	private static final String[] TitulosTabla={"Codigo Asignatura","Descripcion"};
+	private List<ViewAsignatura> asignatura = new ArrayList<ViewAsignatura>();
+
+	
+	  @Override
+	    public String getColumnName(int indice) {
+	        return TitulosTabla[indice];
+	    }
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return TitulosTabla.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int fila, int columna) {
+		// TODO Auto-generated method stub
+ 
+		return null;
+	}
+	
+	
+}
+	
+
