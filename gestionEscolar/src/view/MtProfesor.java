13fbba293b2596b5b53f05502babd8072c5bd4fb
lesman Ivan Matute <ivanmatute576@yahoo.com>
2015-07-31 10:31:54 -0600

    vista

---
+++ gestionEscolar/src/view/MtProfesor.java
@@ -0,0 +1,42 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+
+public class MtProfesor extends AbstractTableModel {
+	private String []TituloTabla={"RNE","Nombres","Apellido","Edad","Telefono","Correo"};
+	private List<ViewProfesores> profesores = new ArrayList<ViewProfesores>();
+	
+	
+	@Override
+	public String getColumnName( int Indice) {
+		// TODO Auto-generated method stub
+		return TituloTabla[Indice];
+	}
+	
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 6;
+	}
+	
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+		
+	}
+	@Override
+	public Object getValueAt(int fila, int columna) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+	
+	
+}
--- gestionEscolar/src/view/ViewAgregarSeccion.java
+++ gestionEscolar/src/view/ViewAgregarSeccion.java
@@ -80,6 +80,5 @@ public class ViewAgregarSeccion extends JFrame{
 		getContentPane().add(btnGuardar);
 		
 		
-		
 	}
 }
