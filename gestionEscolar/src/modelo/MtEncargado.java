2438a99b27d8e4c3ff34630a6d002a8abad6a2ae
Nicole Sosa <lsosa@LAB2-PC9.hdm.edu>
2015-08-03 11:45:17 -0600

    nuevas clases

--- gestionEscolar/src/modelo/MtEncargado.java
+++ gestionEscolar/src/modelo/MtEncargado.java
@@ -1,5 +1,40 @@
 package modelo;
 
-public class MtEncargado {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import view.ViewEncargados;
+
+public class MtEncargado extends AbstractTableModel {
+	
+	private String []TituloTabla={"Codigo","Nombre","Apellido", "Telefono", "Direccion"};
+	private List<ViewEncargados> Encargados= new ArrayList<ViewEncargados>();
+
+	
+	@Override
+	public String getColumnName(int indice) {
+		// TODO Auto-generated method stub
+		return TituloTabla[indice];
+	}
+	
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return TituloTabla.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int fila, int columna) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- gestionEscolar/src/view/ViewEncargados.java
+++ gestionEscolar/src/view/ViewEncargados.java
@@ -5,10 +5,15 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 
+import modelo.MtEncargado;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
 public class ViewEncargados extends JFrame {
 	
 	private JTable mytabla;
-	private JScrollPane scrollpane;
+	private JScrollPane scrollPane;
+	private MtEncargado mymodelo;
 	
 	ViewEncargados(){
 		getContentPane().setLayout(null);
@@ -17,10 +22,16 @@ public class ViewEncargados extends JFrame {
 		mytabla.setModel(mymodelo);
 		mytabla.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		
-		mymodelo=new 
 		
-		JScrollPane scrollPane = new JScrollPane();
+		mymodelo=new MtEncargado();
+		mytabla=new JTable();
+		mytabla.setModel(mymodelo);
+		scrollPane.setViewportView(mytabla);
+		
+	    scrollPane = new JScrollPane();
 		scrollPane.setBounds(37, 55, 361, 195);
 		getContentPane().add(scrollPane);
-	}
+		scrollPane.setViewportView(mytabla);
+		
+			}
 }
