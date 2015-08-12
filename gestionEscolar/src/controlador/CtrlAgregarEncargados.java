3ef6daa6dd04e2f7cf6f316e48eab77afc614f3a
Elsy Cruz <mejiaelsy97@gmail.com>
2015-08-12 11:07:26 -0600

    realice cambios en estas clases

---
+++ gestionEscolar/src/controlador/CtrlAgregarEncargados.java
@@ -0,0 +1,54 @@
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+import modelo.EncargadosPadres;
+import view.ViewEncargadoResponsable;
+
+public class CtrlAgregarEncargados implements ActionListener {
+	
+	 private ViewEncargadoResponsable view;
+	  private EncargadosPadres myEncargado;
+	
+	    
+	public CtrlAgregarEncargados(ViewEncargadoResponsable v){	
+		view=v;
+		myEncargado = new EncargadosPadres();	
+		
+	}
+	
+	
+	public void Guardar(){
+		String rnp= view.gettxtNoIdentidad().getText();
+		myEncargado.setRnp(rnp); 
+		String nombre= view.gettxtNombre().getText();
+		myEncargado.setNombre(nombre); 
+		String apellido= view.gettxtApellido().getText();
+		myEncargado.setApellido(apellido);
+		String telefono= view.gettxtTelefono().getText();
+		myEncargado.setTelefono(telefono);
+		String direccion=view.gettxtDireccion().getText();
+		myEncargado.setDireccion(direccion); 
+	}
+	
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String comando= e.getActionCommand();
+		
+		switch(comando){
+		
+		case "GUARDAR": 
+			JOptionPane.showMessageDialog(null, "Se Guardo Correctamente");
+		break;
+		
+		case "CANCELAR":
+			JOptionPane.showMessageDialog(null, "Se Cancelo"); 
+		break;
+		}
+	}
+	
+
+}
