8bd96a8fda96092f241a55b3887021a977fa9590
Nicole Sosa <lsosa@LAB2-PC9.hdm.edu>
2015-08-25 08:44:06 -0600

    guarda en la base de datos

--- gestionEscolar/src/controlador/CtrlAgregarModalidad.java
+++ gestionEscolar/src/controlador/CtrlAgregarModalidad.java
@@ -5,27 +5,54 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JOptionPane;
 
+import view.ViewAgregarModalidad;
 import modelo.Modalidad;
+import modelo.connBD.ModalidadDao;
 
 public class CtrlAgregarModalidad implements ActionListener {
 	
 	private Modalidad myModalidad;
+	private ViewAgregarModalidad view;
+	private ModalidadDao myDao;
 	
-	public CtrlAgregarModalidad(){
+	public CtrlAgregarModalidad(ViewAgregarModalidad v)
+	{
+		view= v;
+		myModalidad= new Modalidad();
 		
 	}
-
+	
+	private void Guardar(){
+		String Modalidad= view.getTxtIdModalidad().getText();
+		myModalidad.setCodigoModalidad(Modalidad);
+		String Nombre= view.getTxtNombre().getText();
+		myModalidad.setNombre(Nombre);
+		
+		ModalidadDao myDao=new ModalidadDao();
+		boolean resultado= myDao.registrar(myModalidad);
+		
+		if (resultado==true){
+			JOptionPane.showMessageDialog(view, "se guardo");
+		}
+		else{
+			JOptionPane.showMessageDialog(null, "No se guardo");
+		}
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 		String comando= e.getActionCommand();
 		switch(comando){
-		case "GUARDAR": JOptionPane.showMessageDialog(null, "Se ha guardado");
+		case "GUARDAR": 
+			           Guardar();
 			break;
 			
 			
-		case "CANCELAR": JOptionPane.showMessageDialog(null, "se ha cancelado");
+		case "CANCELAR": 
+			            System.exit(0);
+			
 			break;
 		}
 		
