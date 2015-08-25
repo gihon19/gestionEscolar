9f7a5198140e43a7f1e118be7f62eb13096996e3
lesman Ivan Matute <ivanmatute576@yahoo.com>
2015-08-25 08:07:21 -0600

    AgregarAsignatura lista

--- gestionEscolar/src/view/ViewAgregarClase.java
+++ gestionEscolar/src/view/ViewAgregarClase.java
@@ -12,44 +12,73 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 //Importaciones de otras clases
 
+
+
+import view.botones.BotonCancelar;
+import view.botones.BotonGuardar;
+import controlador.CtrAgregarAsignatura;
+
 public class ViewAgregarClase extends JDialog {
 	//Variables.
-	private JTextField textField;
-	private JTextField textField_1;
-	private JButton btnGuardar;
-	//Constructor
-	public ViewAgregarClase(Window v) {
-		//espara agregar un tema Arriba, en la ventana.
-		super(v,"Agregar Modalidad",Dialog.ModalityType.DOCUMENT_MODAL);
-		//Metodos.
-		getContentPane().setLayout (null);
+		private JTextField txtIdClase;
+		private JTextField txtDescripcion;
+		private JButton btnGuardar;
+		private JButton btnCancelar;
 		
-		//Cuadro para insertar texto
-		textField = new JTextField();
-		textField.setBounds(20, 46, 86, 20);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+		//Constructor
+		public ViewAgregarClase(Window v) {
+			
+			//espara agregar un tema Arriba, en la ventana.
+			super(v,"Agregar Modalidad",Dialog.ModalityType.DOCUMENT_MODAL);
+			//Metodos.
+			
+			getContentPane().setLayout (null);
+			
+			//Cuadro para insertar texto
+			txtIdClase = new JTextField();
+			txtIdClase.setBounds(20, 46, 228, 20);
+			getContentPane().add(txtIdClase);
+			txtIdClase.setColumns(10);
+			
+			txtDescripcion = new JTextField();
+			txtDescripcion.setBounds(20, 125, 228, 20);
+			getContentPane().add(txtDescripcion);
+			txtDescripcion.setColumns(10);
+			
+			//agregar texto
+			JLabel lblIdClase = new JLabel("Id Clase");
+			lblIdClase.setBounds(20, 29, 46, 14);
+			getContentPane().add(lblIdClase);
+			
+			//Agregar Boton
+			btnGuardar = new BotonGuardar();
+			btnGuardar.setLocation(20, 182);
+			getContentPane().add(btnGuardar);
+			
+			//agregar texto
+			JLabel lblNombre = new JLabel("Descripcion");
+			lblNombre.setBounds(20, 104, 54, 20);
+			getContentPane().add(lblNombre);
+			
+			//Agregar Boton
+			btnCancelar = new BotonCancelar();
+			btnCancelar.setLocation(181, 182);
+			getContentPane().add(btnCancelar);
+		}
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(20, 125, 268, 20);
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		//agregar texto
-		JLabel lblIdClase = new JLabel("Id Clase");
-		lblIdClase.setBounds(20, 29, 46, 14);
-		getContentPane().add(lblIdClase);
-		//Agregar Boton
-		btnGuardar = new JButton("Guardar");
-		btnGuardar.setBounds(20, 182, 109, 30);
-		getContentPane().add(btnGuardar);
-		//agregar texto
-		JLabel lblNombre = new JLabel("Nombre");
-		lblNombre.setBounds(20, 104, 54, 20);
-		getContentPane().add(lblNombre);
-		//Agregar Boton
-		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.setBounds(164, 182, 109, 30);
-		getContentPane().add(btnCancelar);
-	}
+		public void conectarCtrl(CtrAgregarAsignatura c)
+		{
+			btnGuardar.addActionListener(c);
+			btnGuardar.setActionCommand("GUARDAR");
+			btnCancelar.addActionListener(c);
+			btnCancelar.setActionCommand("CANCELAR");
+		}
+		
+		public JTextField  getTxtIdClase(){
+			return txtIdClase;
+		}
+		public JTextField getTxtDescripcion(){
+			return txtDescripcion;
+		}
 }
 
